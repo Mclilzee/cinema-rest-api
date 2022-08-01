a9c594733b370979ab5519b96f5ddcfa431d3d40
Mclilzee <mc.lilzee@hotmail.com>
2022-08-01 02:14:51 +0200

    Seat class modification

--- src/main/java/come/projects/cinemarestapi/Seat.java
+++ src/main/java/come/projects/cinemarestapi/Seat.java
@@ -3,10 +3,14 @@ package come.projects.cinemarestapi;
 public class Seat {
     private final int row;
     private final int column;
+    private final int price;
+    private boolean purchased;
 
     public Seat(int row, int column) {
         this.row = row;
         this.column = column;
+        this.purchased = false;
+        this.price = this.row <= 4 ? 10 : 8;
     }
 
     public int getRow() {
@@ -15,5 +19,17 @@ public class Seat {
 
     public int getColumn() {
         return this.column;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public boolean isPurchased() {
+        return purchased;
+    }
+
+    public void setPurchased(boolean purchased) {
+        this.purchased = purchased;
     }
 }
